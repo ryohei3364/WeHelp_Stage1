0326fb86271d09512fa87c2e8c0a6a2e3392c202
Chih Pin Chien <ryohei3364@gmail.com>
2025-01-09 12:14:19 +0800

    # 3 revised index.html title name and adjusted layout.js with the problem of menu showup concerning different screen width

--- script/layout.js
+++ script/layout.js
@@ -3,14 +3,24 @@ function toggleMenu() {
     var close = document.querySelector('.mobile-close'); 
     var mobileMenu = document.querySelector('.mobile-menu');
 
+    window.addEventListener('resize', function(event) {
+        if (window.innerWidth >= 601) {
+            menu.style.display = "inline-block"; // 顯示menu選單
+        } else {
+            menu.style.display = "none"; // 隱藏menu選單
+            mobileMenu.style.display = "inline-block"; // 顯示menu按鈕  
+        }       
+    }, true);
+
     // 檢查menu是否為隱藏或未設定display屬性
     if (menu.style.display === "none" || menu.style.display === "") {
-        menu.style.display = "flex"; // 顯示menu選單
-        close.style.display = "block"; // 顯示close按鈕
+        menu.style.display = "inline-block"; // 顯示menu選單
+        close.style.display = "inline-block"; // 顯示close按鈕
         mobileMenu.style.display = "none"; // 隱藏menu按鈕
     } else {
         menu.style.display = "none"; // 隱藏menu選單
         close.style.display = "none"; // 隱藏close按鈕
-        mobileMenu.style.display = "block"; // 顯示menu按鈕
+        mobileMenu.style.display = "inline-block"; // 顯示menu按鈕
     }
+
 }
