83706100b78a2f1ff5a94970d9e0d0540eccb31c
Chih Pin Chien <ryohei3364@gmail.com>
2025-01-25 01:42:34 +0800

    # 3 update week3 assignment: revised task 2 code

--- week3/task-2.py
+++ week3/task-2.py
@@ -1,14 +1,19 @@
-import requests, urllib.parse, csv, re
+import urllib.request, json, csv, re
 from bs4 import BeautifulSoup
 
 URL = 'https://www.ptt.cc/bbs/Lottery/index.html'
-OVER_18_HEADERS = {'cookie': 'over18 = 1;'}
 NUM_PAGE = 3
-
+  
 def parse(url):
-  '''use beautiful soup to parse html file'''
-  response = requests.get(url, headers = OVER_18_HEADERS)
-  soup = BeautifulSoup(response.text, "html.parser")
+  '''use urllib and beautiful soup to parse html file'''
+  request = urllib.request.Request(url, headers = {
+    'cookie': 'over18 = 1',
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36"
+  })
+  with urllib.request.urlopen(request) as response:
+    data = response.read().decode("utf-8")
+
+  soup = BeautifulSoup(data, 'html.parser')
   return soup
 
 def fetch_data(soup):
@@ -23,7 +28,7 @@ def fetch_data(soup):
       push = element.find('span').get_text()
 
      except AttributeError:
-      print("There's no item with that attribute")
+      print("There's no item with that code")
      
      results.append({'title': title, 'push': push, 'article': 'https://www.ptt.cc/' + article})
   
@@ -31,9 +36,8 @@ def fetch_data(soup):
 
 def parse_next_link(controls):
     '''fetch the link of next page in PTT lottery'''
-    controls = soup.select('.action-bar a.btn.wide')
     next_page = controls[1].get('href') # .get('href') or .attrs['href']
-    number = re.findall(r'\d+', next_page)
+    number = re.findall('\d+', next_page)
     number_int = int(number[0])
     for index in range(number_int, number_int - NUM_PAGE, -1):
       page_link = 'https://www.ptt.cc/bbs/Lottery/index' + str(index) + '.html'
@@ -41,6 +45,7 @@ def parse_next_link(controls):
 
 def publish_time(soup):
   '''link to specific article and parse its datetime'''
+  results = fetch_data(soup)
   dates = []
   for result in results:
     content = parse(result['article'])
@@ -49,7 +54,7 @@ def publish_time(soup):
       dates.append(meta_value[3].text.strip())
     else:
       dates.append(None)
-      '''<span class="article-meta-value">Tue Jan 21 17:45:18 2025</span>'''
+
   return dates
 
 def combine_all_file(soup):
@@ -69,39 +74,18 @@ def combine_all_file(soup):
 
   return combined_results
 
-def remove_delete_article(combined_results):
-    '''remove the deleted articles'''
-    article_counts = {}
-    filtered_results = []
-
-    for line in combined_results:
-        article_url = line['article']
-
-        if article_url in article_counts:
-            article_counts[article_url] += 1
-        else:
-            article_counts[article_url] = 1
-    
-    for line in combined_results:
-        if article_counts[line['article']] == 1:
-            filtered_results.append(line)
-
-    return filtered_results
-
 def combine_article_file():
   '''combine keywords and save file'''
-  combined_results = combine_all_file(soup)
-  filtered_results = remove_delete_article(combined_results)
   article_file = []
 
-  for index in range(len(filtered_results)):
+  for index in range(len(combined_results)):
      article_file.append([
-        filtered_results[index]['title'],
-        filtered_results[index]['push'],
-        filtered_results[index]['date'],
+        combined_results[index]['title'],
+        combined_results[index]['push'],
+        combined_results[index]['date']
      ])
   return article_file
- 
+
 def save_to_json(data, input_name):
    '''Save data in CSV format'''
    with open(f"{input_name}.csv", "a", newline='', encoding="utf-8-sig") as file:
@@ -110,17 +94,15 @@ def save_to_json(data, input_name):
     file.close()
 
 for page in range(NUM_PAGE):
-   '''parse the html content'''
+   '''parse the html content with the setup number of pages'''
    soup = parse(URL)
-   results = fetch_data(soup)
-   article_file = combine_article_file()
    combined_results = combine_all_file(soup)
-   filtered_results = remove_delete_article(combined_results)
+   article_file = combine_article_file()
    page_link = parse_next_link(controls = soup.select('.action-bar a.btn.wide'))
-   '''<div class="action-bar"><a class="btn wide" href="/bbs/Lottery/index1.html">最舊</a>...</div>'''
    URL = page_link
    save_to_json(article_file, 'article')
    
+
 
 
 
